3e3b5ce86d353a4eb0b8ff895acead24b3362f2f
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2026-05-27 23:21:17 +0200

    chore: log DISPLAY

--- container-overlay/opt/gow/startup-app.sh
+++ container-overlay/opt/gow/startup-app.sh
@@ -4,7 +4,7 @@ set -e
 source /opt/gow/bash-lib/utils.sh
 
 gow_log "Starting Wolf UI"
-gow_log "Display backend: GDK_BACKEND=${GDK_BACKEND} WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-} XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-}"
+gow_log "Display backend: DISPLAY=${DISPLAY:-} GDK_BACKEND=${GDK_BACKEND} WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-} XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-}"
 
 source /opt/gow/launch-comp.sh
 
